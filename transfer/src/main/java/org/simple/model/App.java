e709a8144b685349d0b341965afd214cee2546b1
shark0119 <sha4yu0119@gmail.com>
2018-08-17 13:43:28 +0800

    a god damn first comment

---
+++ transfer/src/main/java/org/simple/model/App.java
@@ -0,0 +1,13 @@
+package org.simple.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
